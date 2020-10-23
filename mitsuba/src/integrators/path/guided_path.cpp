4858d7b3da2e8b469231a3c3e3057243c159d9bc
Sunrise <sunrisewng@gmail.com>
2020-10-23 10:00:58 +0200

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -1470,14 +1470,14 @@ public:
                 if(m_isFinalIter){
                     ref<Film> currentIterationFilm = createFilm(film->getCropSize().x, film->getCropSize().y, true);
 
-                    for(int i = 0; i < m_iter; ++i){
+                    for(int curr_iter = 0; curr_iter < m_iter; ++curr_iter){
                         currentIterationFilm->clear();
 
                         ref<ImageBlock> previousSamples = new ImageBlock(Bitmap::ESpectrumAlphaWeight, film->getCropSize(), film->getReconstructionFilter());
                         previousSamples->clear();
 
                         for(std::uint32_t i = 0; i < m_samplePaths->size(); ++i){
-                            if((*m_samplePaths)[i].iter == i){
+                            if((*m_samplePaths)[i].iter == curr_iter){
                                 Spectrum s = (*m_samplePaths)[i].spec * (*m_samplePaths)[i].Li;
                                 previousSamples->put((*m_samplePaths)[i].sample_pos, s, (*m_samplePaths)[i].alpha);
                             }
@@ -1486,7 +1486,7 @@ public:
                         currentIterationFilm->put(previousSamples);
 
                         fs::path scene_path = scene->getDestinationFile();
-                        currentIterationFilm->setDestinationFile(scene_path.parent_path() / std::string("iteration_" + std::to_string(i)), 0);
+                        currentIterationFilm->setDestinationFile(scene_path.parent_path() / std::string("iteration_" + std::to_string(curr_iter)), 0);
 
                         currentIterationFilm->develop(scene, 0.f);
                     }
