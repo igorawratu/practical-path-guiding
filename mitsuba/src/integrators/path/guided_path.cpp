c2b5c548027cb5470a12398cf293114e6622aefe
Sunrise <sunrisewng@gmail.com>
2020-10-29 14:57:04 +0100

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -1134,7 +1134,7 @@ public:
     void resetSDTree() {
         Log(EInfo, "Resetting distributions for sampling.");
 
-        //m_sdTree->refine((size_t)(std::sqrt(std::pow(2, m_iter) * m_sppPerPass / 4) * m_sTreeThreshold), m_sdTreeMaxMemory);
+        m_sdTree->refine((size_t)(std::sqrt(std::pow(2, m_iter) * m_sppPerPass / 4) * m_sTreeThreshold), m_sdTreeMaxMemory);
         m_sdTree->forEachDTreeWrapperParallel([this](DTreeWrapper* dTree) { dTree->reset(20, m_dTreeThreshold); });
     }
 
@@ -1661,7 +1661,7 @@ public:
         int sceneResID, int sensorResID, int samplerResID) {
 
         m_sdTree = std::unique_ptr<STree>(new STree(scene->getAABB()));
-        m_sdTree->subdivide(20);
+        //m_sdTree->subdivide(20);
         m_samplePathMutex = std::unique_ptr<std::mutex>(new std::mutex());
         m_samplePaths = std::unique_ptr<std::vector<PGPath>>(new std::vector<PGPath>());
         m_iter = 0;
