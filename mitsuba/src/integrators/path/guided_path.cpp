c68deb57ae16e053792f791a2f29d3f08285e200
Sunrise <sunrisewng@gmail.com>
2020-11-23 13:03:31 +0100

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -1538,6 +1538,7 @@ public:
             }
 
             (*m_rejSamplePaths)[i].Li = Spectrum(0.f);
+            Spectrum throughput(1.0f);
 
             std::vector<Vertex> vertices;
 
@@ -1556,7 +1557,9 @@ public:
 
                 //rescaling
                 if(acceptProb > 1.f){
-                    (*m_rejSamplePaths)[i].path[j].throughput *= acceptProb;
+                    Spectrum bsdfWeight = (*m_rejSamplePaths)[i].path[j].bsdfVal / (*m_rejSamplePaths)[i].path[j].woPdf;
+                    throughput *= bsdfWeight;
+                    (*m_rejSamplePaths)[i].path[j].throughput = throughput;
                 }
                 //rejection
                 else if(sampler->next1D() > acceptProb){
@@ -1774,9 +1777,9 @@ public:
                         previousSamples->clear();
 
                         for(std::uint32_t i = 0; i < m_rejSamplePaths->size(); ++i){
-                            /*if((*m_rejSamplePaths)[i].path.size() == 0){
+                            if((*m_rejSamplePaths)[i].path.size() == 0){
                                 continue;
-                            }*/
+                            }
 
                             if((*m_rejSamplePaths)[i].iter == curr_iter){
                                 Spectrum s = (*m_rejSamplePaths)[i].spec * (*m_rejSamplePaths)[i].Li;
@@ -1800,7 +1803,7 @@ public:
 
                     #pragma omp parallel for
                     for(std::uint32_t i = 0; i < m_rejSamplePaths->size(); ++i){
-                        /*if((*m_rejSamplePaths)[i].path.size() > 0)*/{
+                        if((*m_rejSamplePaths)[i].path.size() > 0){
                             Spectrum s = (*m_rejSamplePaths)[i].spec * (*m_rejSamplePaths)[i].Li;
                             previousSamples->put((*m_rejSamplePaths)[i].sample_pos, s, (*m_rejSamplePaths)[i].alpha);
                         }                        
