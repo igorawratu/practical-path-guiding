8ad4d686ac51ecc0353d3506eb946c5d3ece4798
Sunrise <sunrisewng@gmail.com>
2021-02-01 13:17:44 +0100

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -2014,7 +2014,7 @@ public:
                 Float nwo = bsf * (*m_samplePaths)[i].path[j].bsdfPdf + (1 - bsf) * dtreePdf;
                 Float reweight = nwo / (*m_samplePaths)[i].path[j].owo;
 
-                if(reweight < 1.f){
+                /*if(reweight < 1.f)*/{
                     (*m_samplePaths)[i].path[j].bsdfVal *= reweight;
                 }
                 (*m_samplePaths)[i].path[j].owo = nwo;
