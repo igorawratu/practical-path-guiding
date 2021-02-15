d761c31e1c3aa82e9e57f927639670578e33d3fe
Sunrise <sunrisewng@gmail.com>
2021-02-15 15:30:05 +0100

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -2793,7 +2793,7 @@ public:
                 
                 sampler->advance();
 
-                if(m_reweight && m_iter >= 4)
+                if(m_reweight)
                 {
                     std::lock_guard<std::mutex> lg(*m_samplePathMutex);
                     m_samplePaths->push_back(std::move(pathRecord));
