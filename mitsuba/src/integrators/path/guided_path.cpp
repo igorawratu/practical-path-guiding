3280b1182aff33889ca57ad987cc2c56e4ae3507
Sunrise <sunrisewng@gmail.com>
2021-02-15 14:02:27 +0100

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -1025,7 +1025,11 @@ public:
     }
 
     float getAugmentedNormalizer(){
-        return current_samples < req_augmented_samples ? float(previous_tree_samples + req_augmented_samples) / float(total_samples) : 1.f;
+        //return current_samples < req_augmented_samples ? float(total_samples) / float(previous_tree_samples + req_augmented_samples) : 1.f;
+        return current_samples < req_augmented_samples ? 
+            float(total_samples) / 
+            (float(current_samples) + previous_tree_samples * getAugmentedMultiplier())
+            : 1.f;
     }
 
     Float pdf(const Vector& dir, bool augment) const {
