314e6d9eb4a6e87d1347a7d5399c2084268eb2ee
Sunrise <sunrisewng@gmail.com>
2021-02-19 13:44:15 +0100

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -3402,8 +3402,14 @@ public:
                     Spectrum eL = throughput * its.Le(-ray.d);
                     recordRadiance(eL);
                     if(!eL.isZero()){
-                        pathRecord.radiance_record.push_back({pathRecord.path.size() == 0 ? -1 : int(pathRecord.path.size()) - 1, eL, 0.f});
-                        rpathRecord.radiance_record.push_back({pathRecord.path.size() == 0 ? -1 : int(rpathRecord.path.size()) - 1, eL, 0.f});
+                        int pos = int(pathRecord.path.size()) - 1;
+                        int rpos = int(rpathRecord.path.size()) - 1;
+                        if(pos > 100 || rpos > 100){
+                            std::cout << pos << " " << rpos << std::endl;
+                        }
+
+                        pathRecord.radiance_record.push_back({pos, eL, 0.f});
+                        rpathRecord.radiance_record.push_back({rpos, eL, 0.f});
                     }
                 }
 
@@ -3413,8 +3419,14 @@ public:
                     recordRadiance(sL);
 
                     if(!sL.isZero()){
-                        pathRecord.radiance_record.push_back({pathRecord.path.size() == 0 ? -1 : int(pathRecord.path.size()) - 1, sL, 0.f});
-                        rpathRecord.radiance_record.push_back({pathRecord.path.size() == 0 ? -1 : int(rpathRecord.path.size()) - 1, sL, 0.f});
+                        int pos = int(pathRecord.path.size()) - 1;
+                        int rpos = int(rpathRecord.path.size()) - 1;
+                        if(pos > 100 || rpos > 100){
+                            std::cout << pos << " " << rpos << std::endl;
+                        }
+
+                        pathRecord.radiance_record.push_back({pos, sL, 0.f});
+                        rpathRecord.radiance_record.push_back({rpos, sL, 0.f});
                     }
                 }
 
