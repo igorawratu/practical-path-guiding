e88b4c4ac997149d51a200ceb595d239b24f64ed
Sunrise <sunrisewng@gmail.com>
2021-03-02 13:18:53 +0100

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -492,7 +492,8 @@ public:
                     Float scalingFactor = pdf < EPSILON && otherPdf < EPSILON ? 1.f : otherPdf / pdf;
 
                     if(pdf < EPSILON && otherPdf > EPSILON){
-                        std::cout << pdf << " " << otherPdf << " " << nodePair.nodeLevel << " " << nodePair.otherNodeLevel << std::endl;
+                        std::cout << pdf << " " << otherPdf << " " << denom << " " << otherDenom << " " <<
+                            nodePair.nodeLevel << " " << nodePair.otherNodeLevel << std::endl;
                     }
                     //std::cout << "leaves: " << otherPdf << " " << otherDenom << " : " << pdf << " " << node.sum(childIdx) << " " << nodePair.nodeFactor << " " << denom << " : " << scalingFactor << std::endl;
                     if(scalingFactor > largestScalingFactor){
