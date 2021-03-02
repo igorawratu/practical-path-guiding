7dbaec66f6a3b06e8db766fcedb04528bee5800d
Sunrise <sunrisewng@gmail.com>
2021-03-02 10:35:18 +0100

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -2272,7 +2272,7 @@ public:
                     break;
                 }
 
-                (*m_rejSamplePaths)[i].path[j].woPdf = newWoPdf;
+                //(*m_rejSamplePaths)[i].path[j].woPdf = newWoPdf;
                 //(*m_rejSamplePaths)[i].path[j].bsdfVal *= dTree->getAugmentedNormalizer() * dTree->getAugmentedMultiplier();
 
                 Spectrum bsdfWeight = (*m_rejSamplePaths)[i].path[j].bsdfVal / (*m_rejSamplePaths)[i].path[j].woPdf;
