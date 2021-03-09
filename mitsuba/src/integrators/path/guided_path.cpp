e9c348949bba2e92e0a2ce765bc7d9b65a277326
Sunrise <sunrisewng@gmail.com>
2021-03-09 11:32:02 +0100

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -2372,7 +2372,7 @@ public:
             for(std::uint32_t j = 0; j < (*m_currAugmentedPaths)[i].path.size(); ++j){
                 Vector dTreeVoxelSize;
                 DTreeWrapper* dTree = m_sdTree->dTreeWrapper((*m_currAugmentedPaths)[i].path[j].ray.o, dTreeVoxelSize);
-                (*m_currAugmentedPaths)[i].path[j].bsdfVal *= /*dTree->getAugmentedNormalizer() * */dTree->getAugmentedMultiplier();
+                (*m_currAugmentedPaths)[i].path[j].bsdfVal *= dTree->getAugmentedNormalizer() * dTree->getAugmentedMultiplier();
 
                 Spectrum bsdfWeight = (*m_currAugmentedPaths)[i].path[j].bsdfVal / (*m_currAugmentedPaths)[i].path[j].woPdf;
                 throughput *= bsdfWeight;
@@ -2426,7 +2426,7 @@ public:
 
                     Spectrum L = (*m_currAugmentedPaths)[i].nee_records[j].L;
                     Float pdf = (*m_currAugmentedPaths)[i].nee_records[j].pdf;
-                    (*m_currAugmentedPaths)[i].nee_records[j].bsdfVal *= /*dTree->getAugmentedNormalizer() * */dTree->getAugmentedMultiplier();
+                    (*m_currAugmentedPaths)[i].nee_records[j].bsdfVal *= dTree->getAugmentedNormalizer() * dTree->getAugmentedMultiplier();
                     Spectrum bsdfVal = (*m_currAugmentedPaths)[i].nee_records[j].bsdfVal;
                     
                     int current_level = 0;
@@ -2626,6 +2626,10 @@ public:
                 Float acceptProb = newWoPdf / (*m_rejSamplePaths)[i].path[j].woPdf;
                 Float oldWo = (*m_rejSamplePaths)[i].path[j].woPdf;
                 (*m_rejSamplePaths)[i].path[j].woPdf = newWoPdf;
+
+                if(std::abs(dTree->getAugmentedNormalizer() - 1.f) < EPSILON){
+                    std::cout << dTree->getAugmentedNormalizer() << std::endl;
+                }
 
                 (*m_rejSamplePaths)[i].path[j].bsdfVal *= dTree->getAugmentedNormalizer();
                 if(sampler->next1D() > acceptProb){
