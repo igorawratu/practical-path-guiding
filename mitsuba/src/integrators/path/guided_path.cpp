3315c3d1b18be07151951ee4407674265f060ef5
Sunrise <sunrisewng@gmail.com>
2021-03-12 10:07:20 +0100

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -2882,7 +2882,11 @@ public:
 
                 if(reuseSamples){
                     std::uint32_t path_pos = start_pos + i * m_sppPerPass + j;
-                    std::cout << "PATH POS: " << path_pos << " : " << m_samplePaths->size() << " : " << m_currAugmentedPaths->size() << std::endl;
+
+                    if(path_pos >= m_samplePaths.size()){
+                        std::cout << "PATH POS: " << path_pos << " : " << m_samplePaths->size() << std::endl;    
+                    }
+                    
 
                     if(m_reweight || m_reject || m_rejectReweight){
                         RPath path;
