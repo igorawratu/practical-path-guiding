4ebdc2b57690dcebc2aeffdcf1b7bfa46db232e6
Sunrise <sunrisewng@gmail.com>
2021-03-22 10:36:25 +0100

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -2147,7 +2147,7 @@ public:
                     break;
                 }
                 else{
-                    Float rw_scale = std::max(1.f, newWoPdf / oldWo);
+                    Float rw_scale = newWoPdf / oldWo;//std::max(1.f, newWoPdf / oldWo);
                     (*m_samplePaths)[i].path[j].sc *= rw_scale;
                     Spectrum bsdfWeight = (*m_samplePaths)[i].path[j].bsdfVal / newWoPdf;
                     throughput *= bsdfWeight;
