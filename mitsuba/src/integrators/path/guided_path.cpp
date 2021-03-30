27eb97064ac9110033a1f6dc8685debb04b718e6
Sunrise <sunrisewng@gmail.com>
2021-03-30 13:34:53 +0200

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -2832,7 +2832,7 @@ public:
         if(m_staticSTree){
             m_sdTree->subdivide(16);
         }
-
+        
         m_samplePathMutex = std::unique_ptr<std::mutex>(new std::mutex());
         m_samplePaths = std::unique_ptr<std::vector<RPath>>(new std::vector<RPath>());
         m_currAugmentedPaths = std::unique_ptr<std::vector<RPath>>(new std::vector<RPath>());
@@ -2930,8 +2930,8 @@ public:
         if (!sensor->getFilm()->hasAlpha()) // Don't compute an alpha channel if we don't have to
             queryType &= ~RadianceQueryRecord::EOpacity;
 
-        bool reuseSamples = m_iter >= m_strategyIterationActive && (((m_reweight || m_rejectReweight || m_reject) && !m_isFinalIter) || 
-            (m_augment || m_rejectAugment || m_reweightAugment));
+        bool reuseSamples = m_iter >= m_strategyIterationActive && (m_reweight || m_rejectReweight || m_reject || 
+            m_augment || m_rejectAugment || m_reweightAugment);
 
         std::unique_ptr<std::vector<RPath>> paths;
 
