5592cc60515e4ae65ba4d1426a45c94f2d169801
Sunrise <sunrisewng@gmail.com>
2021-06-08 14:58:45 +0200

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -1960,7 +1960,7 @@ public:
     float computePdf(const RVertex& vertex, DTreeWrapper*& dTree, Vector& dTreeVoxelSize, float& dTreePdf){
         dTree = m_sdTree->dTreeWrapper(vertex.ray.o, dTreeVoxelSize);
         int curr_level = 0;
-        dTreePdf = dTree->pdf(vertex.ray.d, -1, curr_level);
+        dTreePdf = dTree->pdf(vertex.ray.d, vertex.level, curr_level);
 
         Float bsf = dTree->bsdfSamplingFraction();
 
