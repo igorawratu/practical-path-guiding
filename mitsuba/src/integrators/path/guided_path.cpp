9e88585d98b0bef17cd17e2beaf7721531e2edcc
Sunrise <sunrisewng@gmail.com>
2021-06-11 15:18:58 +0200

    wip

--- mitsuba/src/integrators/path/guided_path.cpp
+++ mitsuba/src/integrators/path/guided_path.cpp
@@ -2169,11 +2169,10 @@ public:
                 Float reweight = nwo / (*m_samplePaths)[i].path[j].woPdf;
 
                 if(reweight < 1.f){
-                    (*m_samplePaths)[i].path[j].sc *= reweight;
+                    (*m_samplePaths)[i].path[j].normalizing_sc *= reweight;
                 }
                 
-                (*m_samplePaths)[i].path[j].normalizing_sc = dTree->getAugmentedMultiplier();
-                //(*m_samplePaths)[i].path[j].sc *= dTree->getAugmentedMultiplier();
+                (*m_samplePaths)[i].path[j].sc *= dTree->getAugmentedMultiplier();
 
                 (*m_samplePaths)[i].path[j].woPdf = nwo;
                 Spectrum bsdfWeight = (*m_samplePaths)[i].path[j].bsdfVal / nwo;
